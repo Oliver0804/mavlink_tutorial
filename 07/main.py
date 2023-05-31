2207c4d76f988124ffceefa2f5060fdbfe23dee2
oliver0804 <icetzsr@gmail.com>
2023-05-31 14:53:28 +0800

    完成整個自動起飛,並添加註解

--- 07/main.py
+++ 07/main.py
@@ -19,7 +19,7 @@
 
 
 
-
+import time
 from pymavlink import mavutil
 
 # 開始一個監聽UDP埠的連線
@@ -42,6 +42,39 @@ print(msg)
 print("從系統接收到的心跳訊息 (系統 %u 元件 %u)" %
       (the_connection.target_system, the_connection.target_component))
 
+# 向飛機發送改變模式的指令，設定模式為 "GUIDED"
+#透過發送一個包含 MAV_CMD_DO_SET_MODE 指令的 COMMAND_LONG 消息來實現的​1​。
+#這裡是每個參數的詳細解釋：
+#target_system: 無人機的系統ID。如果對象是飛行控制器，則此值可以是0。
+#target_component: 無人機的組件ID。如果對象是飛行控制器，則此值可以是0。
+#command: 設為 MAV_CMD_DO_SET_MODE，其值為176。
+#confirmation: 一般設為0。
+#param1: 設為 MAV_MODE_FLAG_CUSTOM_MODE_ENABLED，其值為1，表示啟用自定義模式。
+#param2: 飛行模式的數字。您可以根據不同的無人機類型（例如飛機、直升機、潛水艇、輪式機器人、天線追蹤器等）選擇對應的飛行模式​2​。
+    #對於飛機（Plane），GUIDED模式的數字為15
+    #對於多旋翼機（Copter），GUIDED模式的數字為4
+    #對於水下無人機（Sub），GUIDED模式的數字為4
+    #對於地面車輛（Rover），GUIDED模式的數字為15
+    #對於天線追蹤器（Tracker），並無GUIDED模式
+    #https://github.com/ArduPilot/mavlink/blob/master/message_definitions/v1.0/ardupilotmega.xml
+#param3 到 param7: 這些參數在這個指令中不被使用，所以都設為0。
+print("改變模式為 'GUIDED'")
+the_connection.mav.command_long_send(
+    the_connection.target_system,
+    the_connection.target_component,
+    mavutil.mavlink.MAV_CMD_DO_SET_MODE,
+    0,    # confirmation 
+    1,    # MAV_MODE_FLAG_CUSTOM_MODE_ENABLED
+    4,    # GUIDED mode for Copter
+    0, 0, 0, 0, 0  # unused parameters
+)
+# 接收COMMAND_ACK訊息並輸出
+msg = the_connection.recv_match(type='COMMAND_ACK', blocking=True)
+print(msg)
+
+
+
+
 # 向飛機再次發送解鎖指令
 print("解鎖")
 the_connection.mav.command_long_send(the_connection.target_system, the_connection.target_component,
@@ -50,6 +83,7 @@ the_connection.mav.command_long_send(the_connection.target_system, the_connectio
 msg = the_connection.recv_match(type='COMMAND_ACK', blocking=True)
 print(msg)
 
+time.sleep(1)
 # 向飛機發送起飛指令，目標高度為40
 print("起飛")
 the_connection.mav.command_long_send(the_connection.target_system, the_connection.target_component,
